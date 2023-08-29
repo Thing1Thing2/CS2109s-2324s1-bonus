0815e1bcf47594cd3e567593d4a864e8661136e6
Han Liang Wee Eric <eric.aka.darth_vader@hotmail.com>
2023-08-29 18:44:38 +0800

    Updated Bonus T1

---
+++ T01/tutorial1.py
@@ -0,0 +1,27 @@
+graph = {
+    'S': {('A', 1), ('B', 5),  ('C', 15)},
+    'A': {('G', 10), ('S', 1)},
+    'B': {('G', 5), ('S', 5)},
+    'C': {('G', 5), ('S', 15)},
+    'G': set()
+}
+
+from priority_queue import PriorityQueue
+from collections import defaultdict
+
+def uniform_cost_search(graph, inital_node, goal_test, is_tree):
+
+    # frontier = PriorityQueue('min', f)
+    raise NotImplementedError
+
+print("=====")
+print("Tree")
+print("=====")
+assert(uniform_cost_search(graph, 'S', lambda n: n=='G', is_tree=True)=="SASASBSBG")
+# You might get a different path due to popping of B instead.
+# A(5) B(5) B(7) B(9) G(11) G(13) C(15) C(17) C(19)
+
+print("=====")
+print("Graph")
+print("=====")
+assert(uniform_cost_search(graph, 'S', lambda n: n=='G', is_tree=False)=="SBG")
