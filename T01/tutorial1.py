7d56bc8bae0ea2ae8fee5479f4effae2eab351b6
Eric Han <Eric.aka.darth_vader@hotmail.com>
2023-08-31 11:20:48 +0800

    Updated code to return path found

--- T01/tutorial1.py
+++ T01/tutorial1.py
@@ -9,6 +9,7 @@ graph = {
 from priority_queue import PriorityQueue
 from collections import defaultdict
 
+# Return the path found
 def uniform_cost_search(graph, inital_node, goal_test, is_tree, is_update):
 
     # frontier = PriorityQueue('min')
@@ -18,9 +19,7 @@ print("=====")
 print("Tree")
 print("=====")
 print(p:=uniform_cost_search(graph, 'S', lambda n: n=='G', is_tree=True, is_update=False))
-assert(p=="SASASBSBG")
-# You might get a different path due to popping of B instead.
-# A(5) B(5) B(7) B(9) G(11) G(13) C(15) C(17) C(19)
+assert(p=="SBG")
 
 print("=====")
 print("Graph")
